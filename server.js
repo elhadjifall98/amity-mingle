857a4fa7957ed8b8c278eb1461c16e4b0e7f9a88
elzoo <elhadji_fall98@outlook.fr>
2023-12-21 22:09:30 +0100

    start1

--- server.js
+++ server.js
@@ -3,31 +3,22 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const userRoutes = require('./routes/user.routes');
 const postRoutes = require('./routes/post.routes');
-require('dotenv').config({path: './config/.env'});
+require('dotenv').config({ path: './config/.env' });
 require('./config/db');
-const {checkUser, requireAuth} = require('./middleware/auth.middleware');
+const { checkUser, requireAuth } = require('./middleware/auth.middleware');
 const cors = require('cors');
 
 const app = express();
 
-const corsOptions = {
-  origin: process.env.CLIENT_URL,
-  credentials: true,
-  'allowedHeaders': ['sessionId', 'Content-Type'],
-  'exposedHeaders': ['sessionId'],
-  'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  'preflightContinue': false
-}
-app.use(cors(corsOptions));
-
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 // jwt
-app.get('*', checkUser);
+app.use(checkUser);
 app.get('/jwtid', requireAuth, (req, res) => {
-  res.status(200).send(res.locals.user._id)
+  res.status(200).send(res.locals.user._id);
 });
 
 // routes
@@ -39,4 +30,4 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
-})
+});
