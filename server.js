4aba2d3dce9fd97cf9da00aa80d799f7ed72b20c
elzoo <elhadji_fall98@outlook.fr>
2023-12-24 23:12:10 +0100

    initial commit12

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 const express = require('express');
-const http = require('http');
-const webSocket = require ('ws');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const userRoutes = require('./routes/user.routes');
@@ -11,8 +9,6 @@ const {checkUser, requireAuth} = require('./middleware/auth.middleware');
 const cors = require('cors');
 
 const app = express();
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
 
 const corsOptions = {
   origin: process.env.CLIENT_URL,
@@ -30,37 +26,6 @@ app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://quiet-oasis-36311-7ecc0ef1ffe1.herokuapp.com');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  // ...
-  next();
-});
-
-wss.on('connection', (ws, req) => {
-  // Récupérer le token depuis le cookie
-  const token = req.cookies.jwt;
-
-  // Faire quelque chose avec le token (par exemple, vérifier l'authentification)
-  if (token) {
-    // Authentification réussie, gérer la connexion WebSocket
-    console.log('WebSocket connected with token:', token);
-  } else {
-    // Authentification échouée, fermer la connexion WebSocket
-    console.log('WebSocket connection without token. Closing connection.');
-    ws.close();
-  }
-
-  // Gérer les messages, etc.
-  ws.on('message', (message) => {
-    console.log(`Received message: ${message}`);
-  });
-
-  // Gérer la fermeture de la connexion
-  ws.on('close', () => {
-    console.log('WebSocket closed');
-  });
-});
 
 // jwt
 app.get('*', checkUser);
