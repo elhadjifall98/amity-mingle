17b8a9c1287953ca34fe3df86fbbd6a788859a06
elzoo <elhadji_fall98@outlook.fr>
2023-12-24 18:15:29 +0100

    initial commit12

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,19 +1,18 @@
 const jwt = require("jsonwebtoken");
 const UserModel = require("../models/user.model");
 
-module.exports.checkUser = (req, res, next) => {
+const checkUser = (req, res, next) => {
   const token = req.cookies.jwt;
   if (token) {
     jwt.verify(token, process.env.TOKEN_SECRET, async (err, decodedToken) => {
       if (err) {
         res.locals.user = null;
         // res.cookie("jwt", "", { maxAge: 1 });
-        next();
       } else {
         let user = await UserModel.findById(decodedToken.id);
         res.locals.user = user;
-        next();
       }
+      next();
     });
   } else {
     res.locals.user = null;
@@ -21,13 +20,13 @@ module.exports.checkUser = (req, res, next) => {
   }
 };
 
-module.exports.requireAuth = (req, res, next) => {
+const requireAuth = (req, res, next) => {
   const token = req.cookies.jwt;
   if (token) {
     jwt.verify(token, process.env.TOKEN_SECRET, async (err, decodedToken) => {
       if (err) {
         console.log(err);
-        res.send(200).json('no token')
+        res.status(401).json({ message: 'Unauthorized' });
       } else {
         console.log(decodedToken.id);
         next();
@@ -35,5 +34,8 @@ module.exports.requireAuth = (req, res, next) => {
     });
   } else {
     console.log('No token');
+    res.status(401).json({ message: 'Unauthorized' });
   }
 };
+
+module.exports = { checkUser, requireAuth };
