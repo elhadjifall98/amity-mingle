3e5ccc1481dee6ccca693a790d95e115153f4556
elzoo <elhadji_fall98@outlook.fr>
2023-12-24 17:14:49 +0100

    initial commit12

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,38 +1,39 @@
 const jwt = require("jsonwebtoken");
 const UserModel = require("../models/user.model");
 
-module.exports.checkUser = async (req, res, next) => {
+module.exports.checkUser = (req, res, next) => {
   const token = req.cookies.jwt;
   if (token) {
-    try {
-      const decodedToken = jwt.verify(token, process.env.TOKEN_SECRET);
-      let user = await UserModel.findById(decodedToken.id);
-      res.locals.user = user;
-    } catch (err) {
-      res.locals.user = null;
-      res.cookie("jwt", "", { maxAge: 1 }); // Décommenter si vous souhaitez déconnecter l'utilisateur en cas d'erreur
-    }
+    jwt.verify(token, process.env.TOKEN_SECRET, async (err, decodedToken) => {
+      if (err) {
+        res.locals.user = null;
+        // res.cookie("jwt", "", { maxAge: 1 });
+        next();
+      } else {
+        let user = await UserModel.findById(decodedToken.id);
+        res.locals.user = user;
+        next();
+      }
+    });
   } else {
     res.locals.user = null;
+    next();
   }
-  next();
 };
 
 module.exports.requireAuth = (req, res, next) => {
   const token = req.cookies.jwt;
   if (token) {
-    try {
-      const decodedToken = jwt.verify(token, process.env.TOKEN_SECRET);
-      console.log(decodedToken.id);
-      next();
-    } catch (err) {
-      console.error(err);
-      res.status(401).json({ message: 'Unauthorized' });
-      // Vous pouvez également appeler next(err) ici si vous avez un gestionnaire d'erreurs global
-    }
+    jwt.verify(token, process.env.TOKEN_SECRET, async (err, decodedToken) => {
+      if (err) {
+        console.log(err);
+        res.send(200).json('no token')
+      } else {
+        console.log(decodedToken.id);
+        next();
+      }
+    });
   } else {
     console.log('No token');
-    res.status(401).json({ message: 'Unauthorized' });
-    // Vous pouvez également appeler next(new Error('No token')) ici si vous avez un gestionnaire d'erreurs global
   }
 };
